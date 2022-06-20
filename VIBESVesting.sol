fe21d2458ccac1c80bbfc6775a7ba0a98cbd2175
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2022-06-20 12:13:23 +0530

    Update VIBESVesting.sol

--- VIBESVesting.sol
+++ VIBESVesting.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.11;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./IERC20.sol";
+import "./SafeERC20.sol";
+import "./ReentrancyGuard.sol";
+import "./Ownable.sol";
+import "./SafeMath.sol";
 
 
 contract VIBESVesting is Ownable, ReentrancyGuard{
