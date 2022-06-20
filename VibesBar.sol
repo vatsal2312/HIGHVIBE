4e73f33b06b880d730406667bc2714c98339e7ec
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2022-06-20 12:14:19 +0530

    Update VibesBar.sol

--- VibesBar.sol
+++ VibesBar.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import "./ERC20Upgradeable.sol";
+import "./OwnableUpgradeable.sol";
+import "./SafeMathUpgradeable.sol";
 import "./VIBESToken.sol";
 
 // VibesBar with Governance.
