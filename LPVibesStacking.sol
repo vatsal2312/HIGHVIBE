38743e61a4eb9eacbdca72b8627bb4fa0a68903b
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2022-06-28 13:28:01 +0530

    Create LPVibesStacking.sol

---
+++ LPVibesStacking.sol
@@ -0,0 +1,316 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AccessControlUpgradeable.sol";
+import "./SafeMathUpgradeable.sol";
+import "./IERC20Upgradeable.sol";
+import "./SafeERC20Upgradeable.sol";
+
+
+import "./VIBESToken.sol";
+import "./VibesBar.sol";
+
+
+// Note that it's ownable and the owner wields tremendous power. The ownership
+// will be transferred to a governance smart contract once VIBE is sufficiently
+// distributed and the community can show to govern itself.
+//
+contract LPVibesStacking is OwnableUpgradeable {
+    using SafeMathUpgradeable for uint256;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount; // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of VIBEs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accVibePerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accVibePerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20Upgradeable lpToken; // Address of LP token contract.
+        uint256 allocPoint; // How many allocation points assigned to this pool. VIBES to distribute per block.
+        uint256 lastRewardBlock; // Last block number that VIBES distribution occurs.
+        uint256 accVibePerShare; // Accumulated VIBES per share, times 1e12. See below.
+    }
+
+    // The VIBES TOKEN!
+    VIBESToken public vibes;
+    // The VibesBar TOKEN!
+    VibesBar public bar;
+    // Dev address.
+    address public devaddr;
+    // VIBES tokens created per block.
+    uint256 public vibePerBlock;
+    // Bonus muliplier for early vibe makers.
+    uint256 public BONUS_MULTIPLIER = 1;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when VIBES mining starts.
+    uint256 public startBlock;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+
+    function initialize(
+        VIBESToken _vibes,
+        VibesBar _bar,
+        address _devaddr,
+        uint256 _vibePerBlock,
+        uint256 _startBlock
+    ) external initializer {
+        __Ownable_init_unchained();
+        vibes = _vibes;
+        bar = _bar;
+        devaddr = _devaddr;
+        vibePerBlock = _vibePerBlock;
+        startBlock = _startBlock;
+
+        // staking pool
+        poolInfo.push(PoolInfo({lpToken: _vibes, allocPoint: 1000, lastRewardBlock: startBlock, accVibePerShare: 0}));
+
+        totalAllocPoint = 1000;
+    }
+
+
+    function updateMultiplier(uint256 multiplierNumber) public onlyOwner {
+        BONUS_MULTIPLIER = multiplierNumber;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(
+        uint256 _allocPoint,
+        IERC20Upgradeable _lpToken,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(
+            PoolInfo({lpToken: _lpToken, allocPoint: _allocPoint, lastRewardBlock: lastRewardBlock, accVibePerShare: 0})
+        );
+        updateStakingPool();
+    }
+
+    // Update the given pool's VIBES allocation point. Can only be called by the owner.
+    function set(
+        uint256 _pid,
+        uint256 _allocPoint,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 prevAllocPoint = poolInfo[_pid].allocPoint;
+        poolInfo[_pid].allocPoint = _allocPoint;
+        if (prevAllocPoint != _allocPoint) {
+            totalAllocPoint = totalAllocPoint.sub(prevAllocPoint).add(_allocPoint);
+            updateStakingPool();
+        }
+    }
+
+    function updateStakingPool() internal {
+        uint256 length = poolInfo.length;
+        uint256 points = 0;
+        for (uint256 pid = 1; pid < length; ++pid) {
+            points = points.add(poolInfo[pid].allocPoint);
+        }
+        if (points != 0) {
+            points = points.div(3);
+            totalAllocPoint = totalAllocPoint.sub(poolInfo[0].allocPoint).add(points);
+            poolInfo[0].allocPoint = points;
+        }
+    }
+
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        return _to.sub(_from).mul(BONUS_MULTIPLIER);
+    }
+
+    // View function to see pending Vibes on frontend.
+    function pendingVibes(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accVibePerShare = pool.accVibePerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 vibeReward = multiplier.mul(vibePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accVibePerShare = accVibePerShare.add(vibeReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accVibePerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 vibeReward = multiplier.mul(vibePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        vibes.mint(devaddr, vibeReward.div(10));
+        vibes.mint(address(bar), vibeReward);
+        pool.accVibePerShare = pool.accVibePerShare.add(vibeReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to LPVibesStacking for VIBES allocation.
+    function deposit(uint256 _pid, uint256 _amount) public {
+        require(_pid != 0, "deposit VIBES by staking");
+
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accVibePerShare).div(1e12).sub(user.rewardDebt);
+            if (pending > 0) {
+                safeVibesTransfer(msg.sender, pending);
+            }
+        }
+        if (_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accVibePerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from LPVibesStacking.
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        require(_pid != 0, "withdraw Vibes by unstaking");
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accVibePerShare).div(1e12).sub(user.rewardDebt);
+        if (pending > 0) {
+            safeVibesTransfer(msg.sender, pending);
+        }
+        if (_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accVibePerShare).div(1e12);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Stake VIBES tokens to LPVibesStacking
+    function enterStaking(uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[0];
+        UserInfo storage user = userInfo[0][msg.sender];
+        updatePool(0);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accVibePerShare).div(1e12).sub(user.rewardDebt);
+            if (pending > 0) {
+                safeVibesTransfer(msg.sender, pending);
+            }
+        }
+        if (_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accVibePerShare).div(1e12);
+
+        bar.mint(msg.sender, _amount);
+        emit Deposit(msg.sender, 0, _amount);
+    }
+
+    // Withdraw VIBES tokens from STAKING.
+    function leaveStaking(uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[0];
+        UserInfo storage user = userInfo[0][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(0);
+        uint256 pending = user.amount.mul(pool.accVibePerShare).div(1e12).sub(user.rewardDebt);
+        if (pending > 0) {
+            safeVibesTransfer(msg.sender, pending);
+        }
+        if (_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accVibePerShare).div(1e12);
+
+        bar.burn(msg.sender, _amount);
+        emit Withdraw(msg.sender, 0, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+
+    // Safe vibe transfer function, just in case if rounding error causes pool to not have enough VIBES.
+    function safeVibesTransfer(address _to, uint256 _amount) internal {
+        bar.safeVibesTransfer(_to, _amount);
+    }
+
+    // Update dev address by the previous dev.
+    function dev(address _devaddr) public {
+        require(msg.sender == devaddr, "dev: wut?");
+        devaddr = _devaddr;
+    }
+
+    function mintVibes(address to, uint256 amount) public onlyOwner {
+        vibes.mint(to, amount);
+    }
+
+     function burnVibes(address to, uint256 amount) external onlyOwner returns (bool) {
+        return vibes.burn(to, amount);
+       
+    }
+
+    function pause() external onlyOwner returns (bool) {
+        return vibes.pause();
+    }
+
+    function unpause() external onlyOwner returns (bool) {
+        return vibes.unpause();
+    }
+
+}
